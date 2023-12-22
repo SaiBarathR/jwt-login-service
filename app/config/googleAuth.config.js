b6241d3117b08add55767650584b9b7c8d597e83
Sai Barath <saibarathr@gmail.com>
2023-12-22 15:57:38 +0530

    Update Google Auth config to use environment variables

--- app/config/googleAuth.config.js
+++ app/config/googleAuth.config.js
@@ -1,5 +1,7 @@
+const dotenv = require('dotenv');
+dotenv.config();
 module.exports = {
-    CLIENT_ID: "",
-    CLIENT_SECRET: "",
-    API_KEY: ""
+    CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    API_KEY: process.env.GOOGLE_API_KEY,
 }
