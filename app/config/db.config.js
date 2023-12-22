86debe84fc01d510232acf31fad1fdbde22f6c59
Sai Barath <saibarathr@gmail.com>
2023-12-23 01:32:11 +0530

    Refactor database configuration to handle SSL connection

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,22 +1,24 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-module.exports = {
-    HOST: process.env.DB_HOST,// add your db host here
-    USER: process.env.DB_USER,// add your db username here
-    PASSWORD: process.env.DB_PASS,// add your db password here
-    DB: process.env.DB_NAME,// add your db name here
-    dialect: process.env.DB_DIALECT,// add your db name here
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+const dialectOptions =
+
+    module.exports = {
+        HOST: process.env.DB_HOST,// add your db host here
+        USER: process.env.DB_USER,// add your db username here
+        PASSWORD: process.env.DB_PASS,// add your db password here
+        DB: process.env.DB_NAME,// add your db name here
+        dialect: process.env.DB_DIALECT,// add your db name here    
+        dialectOptions: process.env.MODE === 'dev' ? {} : {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        },
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
         }
-    },
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-};
+    };
