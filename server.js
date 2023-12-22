2d7f2cd9d0f94deaf00500c46dc04d39b7d35272
Sai Barath <saibarathr@gmail.com>
2023-12-23 01:09:48 +0530

    Fix missing closing parenthesis in server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to the application." });
-}
+});
 
 require('./app/routes/auth.routes')(app);
 require('./app/routes/user.routes')(app);
