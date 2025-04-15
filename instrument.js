6c8ea3a453c431555092953d9df4044694674fe1
Sai Barath <saibarathr@gmail.com>
2025-04-15 15:28:02 +0530

    Add Sentry integration for error tracking and monitoring
    - Introduced `instrument.js` to initialize Sentry with the provided DSN.
    - Updated `server.js` to include Sentry error handling middleware and a test route for triggering errors.
    - Added Sentry CLI and Node SDK as dependencies in `package.json` and `package-lock.json`.
    - Modified the start script to include Sentry sourcemaps upload command.

---
+++ instrument.js
@@ -0,0 +1,6 @@
+// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
+const Sentry = require("@sentry/node");
+
+Sentry.init({
+    dsn: "https://b6990fc25beee1c5e213ea175004346b@o4509156451614720.ingest.de.sentry.io/4509156482547792",
+});
--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+// IMPORTANT: Make sure to import `instrument.js` at the top of your file.
+// If you're using ECMAScript Modules (ESM) syntax, use `import "./instrument.js";`
+require("./instrument.js");
+
+// All other imports below
+// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
+const Sentry = require("@sentry/node");
+
 const express = require("express");
 const cors = require("cors");
 
@@ -31,8 +39,23 @@ app.get("/", (req, res) => {
     res.json({ message: "Welcome to the application." });
 });
 
+app.get("/debug-sentry", function mainHandler(req, res) {
+    throw new Error("My first Sentry error!");
+});
+
 require('./app/routes/auth.routes')(app);
 require('./app/routes/user.routes')(app);
+
+// The error handler must be registered before any other error middleware and after all controllers
+Sentry.setupExpressErrorHandler(app);
+
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+    // The error id is attached to `res.sentry` to be returned
+    // and optionally displayed to the user for support.
+    res.statusCode = 500;
+    res.end(res.sentry + "\n");
+});
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
